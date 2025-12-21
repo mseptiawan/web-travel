836688180d306615d56c116c17a4c2a259a586b7
mseptiawan <mseptiawan017@gmail.com>
2025-12-21 18:50:38 +0700

    dasdsa

--- src/components/FleetGallery.jsx
+++ src/components/FleetGallery.jsx
@@ -11,7 +11,7 @@ const fleets = [
     price: "Mulai Rp 350rb",
   },
   {
-    name: "Kelas Eksekutif (Expan))",
+    name: "Kelas Eksekutif (Expa))",
     img: "/images/armada/expander.avif",
     capacity: "6 Penumpang",
     luggage: "3 Bagasi Besar",
