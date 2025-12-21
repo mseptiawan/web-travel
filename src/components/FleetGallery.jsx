aeb8c1995d670c9afb54df4b0948a577606f9031
mseptiawan <mseptiawan017@gmail.com>
2025-12-21 18:47:41 +0700

    dsad

--- src/components/FleetGallery.jsx
+++ src/components/FleetGallery.jsx
@@ -3,7 +3,7 @@ import { Users, Briefcase, ShieldCheck } from "lucide-react";
 
 const fleets = [
   {
-    name: "Kelas Kelarg (Avanza)",
+    name: "Kelas Kelar (Avanza)",
     img: "/images/armada/avanza.avif",
     capacity: "6-7 Penumpang",
     luggage: "2 Bagasi Besar",
